2d00c2383fa4e0d817273e9f79686b6e510c2215
klamkiewicz <icewind91@wp.pl>
2014-05-09 21:16:47 +0200

    User name test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 
 describe User do
-
+  before {
+    @user = User.new(name: "Example User", email: "user@example.com") 
+  }
+  subject { @user } 
+  
+  it { should respond_to(:name) }
+  #Alternative withouth subject
+  #it "should responed to name" do
+  # expect (@user).to respond_to(:name)
+  #end
 
   
 end
