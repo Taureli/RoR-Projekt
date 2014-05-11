ef648a1b1974ac6b79bf5b12d5a966f58d0d9778
klamkiewicz <icewind91@wp.pl>
2014-05-11 22:28:03 +0200

    Invalid sign-up test

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Jarek Cimoch"
+    email    "konie@example.com"
+    password "koniowo"
+    password_confirmation "koniowo"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,10 +12,27 @@ describe "User pages" do
   end
   
   describe "profile page" do
+	  let(:user) { FactoryGirl.create(:user) }
 	  before { visit user_path(user) }
 
 	  it { should have_content(user.name) }
 	  it { should have_title(user.name) }
+  end
+  
+  #signup using capybara
+  describe "sign-up" do
+    before { visit signup_path }
+    #define the submit as "Create my account"
+    let(:submit) { "Create my account" }
+
+    #If invalid the users count should not increase
+    describe "with invalid input" do
+      it "should not create new user" do
+		#Capybara "makes" the click on submit
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
   end
   
 end
