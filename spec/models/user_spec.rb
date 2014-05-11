48c69e10e0ffa90d66ca65f7536608d354b32ac4
klamkiewicz <icewind91@wp.pl>
2014-05-11 19:26:02 +0200

    Valid test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,12 +6,18 @@ describe User do
   }
   subject { @user } 
   
+  #Check if username is saved
   it { should respond_to(:name) }
   #Alternative withouth subject
   #it "should responed to name" do
   # expect (@user).to respond_to(:name)
   #end
+  
+  #Check if email is saved
   it { should respond_to(:email) }
+  
+  #Check if is valid
+  it { should be_valid }
 
   
 end
