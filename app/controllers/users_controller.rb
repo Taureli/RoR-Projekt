72c5892a7c4f49c7def33fc74e286965a959afa5
jcimoch <cimochjaroslaw@bdimension.com>
2014-05-12 10:18:59 -0700

    autoryzacja impl#1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :signed_in_user, only: [:edit, :update]
   def new
 	@user = User.new
   end
@@ -36,8 +36,18 @@ class UsersController < ApplicationController
   
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-end
+      def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+      end
+
+
+    # Before filters
+
+        def signed_in_user
+          redirect_to signin_url, notice: "Please sign in." unless signed_in?
+        end
+
+
+
+  end
